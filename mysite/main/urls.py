e17770db28d1ccf99abd6af0bd50b7129d3b8638
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-24 21:49:48 +0200

    Created form, template  and view based on this form. Now I can add my own lists.

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,11 +1,12 @@
 from operator import index
 from django import views
 from django.urls import path
-from .views import index, home
+from .views import index, home, create
 
 # In VSC in urlpatterns don't use '/', because it is default in front of url.
 
 urlpatterns = [
     path('<int:id>', index, name='index'),
     path("", home, name='home'),
+    path("create/", create, name="create")
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,14 +1,30 @@
 from unicodedata import name
+from urllib import request
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
-
+from .forms import CreateNewList
 # Create your views here.
+
 
 def index(response, id):
     list_view = ToDoList.objects.get(id=id)
-    name = list_view.name
-    return render(response, template_name="main/index.html", context={"name": name})
+    return render(response, template_name="main/index.html", context={"list_view": list_view})
 
 def home(response):
     return render(response, template_name="main/home.html")
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+        
+        return HttpResponseRedirect("/%i" %t.id)
+
+    else:
+        form = CreateNewList()
+    return render(response, template_name="main/create.html", context={"form": form})
