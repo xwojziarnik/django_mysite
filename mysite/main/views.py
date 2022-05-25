2dfa0fd4ca490a91bf9983d3b4e7a10ba6a7e3ef
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-25 14:58:01 +0200

    Did frontend stuff

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from unicodedata import name
 from urllib import request
 from django.shortcuts import render
@@ -9,6 +10,21 @@ from .forms import CreateNewList
 
 def index(response, id):
     list_view = ToDoList.objects.get(id=id)
+
+    if response.method == "POST":
+        if response.POST.get("save"):
+            for item in list_view.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+            if len(txt) > 2:
+                list_view.item_set.create(text=txt, complete=False)
+            else:
+                print("Invalid")
     return render(response, template_name="main/index.html", context={"list_view": list_view})
 
 def home(response):
