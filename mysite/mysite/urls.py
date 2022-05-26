4a02c45ce2819a3e27e76e3e08bc175a09b00747
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-26 20:19:01 +0200

    Some frontend done, also created register app and created first register view.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from register import views as v
 
 
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
-    
+    path('register/', v.register, name="register"),
 ]
